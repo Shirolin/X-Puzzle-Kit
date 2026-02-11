11a0d6ced83da466897d3012dbf7ff74dc6fb23e
Shirolin <shirolin.com@gmail.com>
2026-02-11 19:17:11 +0800

    chore(config): 更新 iOS 快捷指令版本和链接

--- src/core/config.ts
+++ src/core/config.ts
@@ -45,9 +45,9 @@ export const APP_CONFIG = {
     PWA_URL: "https://x-puzzle-kit.pages.dev/",
     EXTENSION_STORE_URL:
       "https://chromewebstore.google.com/detail/x-puzzle-kit-stitch-split/nadlbdmcfmjinifkoedegmiejfibdikk",
-    IOS_SHORTCUT_VER: "1.0.0",
+    IOS_SHORTCUT_VER: "1.1.0",
     IOS_SHORTCUT_URL:
-      "https://www.icloud.com/shortcuts/b932fef77a3842dd89db1eed4488bf42?source=shortcut",
+      "https://www.icloud.com/shortcuts/3c9d40890abd4fe5a35c59f1dd35e68c?source=shortcut",
     IOS_SHORTCUT_NOTE_KEY: "shortcutUpdateNote",
   },
 };
